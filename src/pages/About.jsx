0f1bbe7e0c954ccf5c1e8a5df9c8d92d051848ab
Jack Whisler <Jack.Whisler@keypathedu.com>
2022-05-25 10:24:00 -0500

    adds pages: about, home, notfound

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details. This app
+        is inspired by the course "React Front To Back" taught by Brad Traversy.
+      </p>
+    </div>
+  );
+}
+
+export default About;
